7eff45ae7426e45905df1f59ad1f97ecc3518282
sandesh93 <sandeshrane93@gmail.com>
2019-04-09 15:20:27 +0530

    added testing version details

--- Monitor_LatestTimeStamp_File_In_Folder.ps1
+++ Monitor_LatestTimeStamp_File_In_Folder.ps1
@@ -1,5 +1,15 @@
 #Script Creation Date: 9th-April-2019
 #Requirment: We have got custome file monitoring requiremnt for AppsOne where we have been asked that application team want alert if specified folder does not contain any .csv file with todays #time stamp.
+##Tested script with below details:
+##PSVersion                      5.1.17134.590                                                                                                                   
+##PSEdition                      Desktop                                                                                                                         
+##PSCompatibleVersions           {1.0, 2.0, 3.0, 4.0...}                                                                                                         
+##BuildVersion                   10.0.17134.590                                                                                                                  
+##CLRVersion                     4.0.30319.42000                                                                                                                 
+##WSManStackVersion              3.0                                                                                                                             
+##PSRemotingProtocolVersion      2.3                                                                                                                             
+##SerializationVersion           1.1.0.1    
+###################################################################################
 
 
 #Mention Directory to Monitor
